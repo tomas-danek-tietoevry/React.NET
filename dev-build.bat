7a871286cbefc9b241e19ace2a56116b719c9c0e
Tomas Danek <tomas.danek@tietoevry.com>
2021-10-29 14:55:07 +0200

    Removed unneeded projects and resources, added SharedAssemblyVersionInfo

--- dev-build.bat
+++ dev-build.bat
@@ -1,6 +1,6 @@
 @echo off
-IF EXIST "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" (
-	"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
+	"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" build.proj
 ) ELSE (
 	"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj
 )
